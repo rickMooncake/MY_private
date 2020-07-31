8d5e7e5c837196ea8d9af1eeb0ecd7e98422b6b3
rickMooncake <long.long.wp@gmail.com>
2020-07-31 17:24:33 +0200

    c04ex20

---
+++ Day05/ex11/ft_str_is_alpha.c
@@ -0,0 +1,17 @@
+int ft_str_is_alpha(char *str)
+{
+	int i;
+
+	if (str[0] == '\0')
+		return (1);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if ((str[i] >= 65 && str[i] <= 90)
+			|| (str[i] >= 97 && str[i] <= 122))
+			i++;
+		else
+			return (0);
+	}
+	return (1);
+}
